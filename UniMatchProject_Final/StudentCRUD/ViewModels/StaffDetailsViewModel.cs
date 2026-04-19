4717f57163233f564d798083e5f3bf0abdafcece
Dewmith Ratnayake <dewmith650@gmail.com>
2026-04-20 00:25:47 +0530

    Add Staff Management module

---
+++ UniMatchProject_Final/StudentCRUD/ViewModels/StaffDetailsViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace StudentCRUD.ViewModels
+{
+    public class StaffDetailsViewModel
+    {
+        public int Id { get; set; }
+        public int StaffId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string UserRole { get; set; }
+        public int UserRoleId { get; set; }
+        public DateTime Created_Date { get; set; }
+        public DateTime Update_Date { get; set; }
+ 
+
+    }
+}
