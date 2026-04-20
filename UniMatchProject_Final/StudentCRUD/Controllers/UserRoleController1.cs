0c9a4008c9f8a23112a0b00020ee6e2edff0b245
Dulan Nethsara <155321720+ApexDN@users.noreply.github.com>
2026-04-20 12:35:11 +0530

    Add files via upload

---
+++ UniMatchProject_Final/StudentCRUD/Controllers/UserRoleController1.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentCRUD.Data;
+using StudentCRUD.Models;
+using System.Linq;
+
+namespace StudentCRUD.Controllers
+{
+    public class UserRoleController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public UserRoleController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.UserRole.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(UserRole UserRole)
+        {
+            _context.UserRole.Add(UserRole);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var UserRole = _context.UserRole.Find(id);
+            return View(UserRole);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(UserRole UserRole)
+        {
+            _context.UserRole.Update(UserRole);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var UserRole = _context.UserRole.Find(id);
+            _context.UserRole.Remove(UserRole);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
