b4152d017ce57ba3741911fa3ae43ae5e2763079
Dulan Nethsara <dulannethsara2@gmail.com>
2026-04-20 09:30:03 +0530

    Add Module and UserRole management - Dulan

---
+++ Desktop/final project/UniMatchProject-Final/UniMatchProject/StudentCRUD/Controllers/ModuleController1.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentCRUD.Data;
+using StudentCRUD.Models;
+using System.Linq;
+
+namespace StudentCRUD.Controllers
+{
+    public class ModuleController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ModuleController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Module.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Module Module)
+        {
+            _context.Module.Add(Module);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var Module = _context.Module.Find(id);
+            return View(Module);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Module Module)
+        {
+            _context.Module.Update(Module);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var Module = _context.Module.Find(id);
+            _context.Module.Remove(Module);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Desktop/final project/UniMatchProject-Final/UniMatchProject/StudentCRUD/Models/Module.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentCRUD.Models
+{
+    public class Module
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ModuleCode { get; set; }
+        public string ModuleName { get; set; }
+        public DateTime Created_Date { get; set; }
+        public DateTime Updated_Date { get; set; }
+}
+}
