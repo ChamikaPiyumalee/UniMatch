ab01f691089b9c547b1650f913d8b6c731e9d265
sandeka09 <sadulap210@gmail.com>
2026-04-20 09:48:07 +0530

    Add files via upload

---
+++ UniMatchProject_Final/StudentCRUD/Controllers/StudentController.cs
@@ -0,0 +1,76 @@
+using Microsoft.AspNetCore.Mvc;
+using StudentCRUD.Data;
+using StudentCRUD.Models;
+using System.Linq;
+
+namespace StudentCRUD.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public StudentController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        private string GenerateStrongPassword(int length = 12)
+        {
+            const string lower = "abcdefghijklmnopqrstuvwxyz";
+            const string upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            const string numbers = "0123456789";
+            const string symbols = "!@#$%^&*()_+";
+
+            var random = new Random();
+
+            string allChars = lower + upper + numbers + symbols;
+
+            return new string(Enumerable.Repeat(allChars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Students.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+            StudentLogin studentLogin =new StudentLogin();
+            studentLogin.StudentId = student.StudentId;
+            studentLogin.Password = GenerateStrongPassword();
+            _context.Students.Add(student);
+            _context.StudentLogin.Add(studentLogin);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var student = _context.Students.Find(id);
+            return View(student);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Student student)
+        {
+            _context.Students.Update(student);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var student = _context.Students.Find(id);
+            _context.Students.Remove(student);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
