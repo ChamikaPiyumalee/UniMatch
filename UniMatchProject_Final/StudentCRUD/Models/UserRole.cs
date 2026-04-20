292a96dfdedcfe206056458d54dc650197815de5
Dulan Nethsara <155321720+ApexDN@users.noreply.github.com>
2026-04-20 12:37:14 +0530

    Add files via upload

---
+++ UniMatchProject_Final/StudentCRUD/Models/UserRole.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentCRUD.Models
+{
+    public class UserRole
+    {
+        [Key]
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+        public DateTime Created_Date { get; set; }
+        public DateTime Updated_Date { get; set; }
+    }
+}
